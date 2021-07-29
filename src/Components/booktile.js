de80debc40bf8584544b8e03faa09fd38f2ad671
Madison Bowden <melybowden@gmail.com>
2021-07-29 13:51:20 -0400

    books api and css

--- src/Components/booktile.js
+++ src/Components/booktile.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState} from 'react'
 import book_not_found from './book_not_found.jpg';
 
-export default function booktile(props) {
+export default function Booktile(props) {
+    const [shelf, setShelf] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert("Added " + props.title + " to " + shelf)
+      axios.post("http://mysterious-plains-09256.herokuapp.com/bookface", 
+        {title: props.title,
+        author: props.author,
+        imageURL: props.imgURL,
+        year: props.year,
+        isbn: props.isbn})
+  }
+
     return (
         <div className="book-list">
-            <img src={book_not_found} alt="book cover" className="book-tile"/>
-            <div>{props.title} by {props.author} &copy; {props.year}</div>
+            <img src={props.imgURL} alt="book cover" className="book-tile"/>
+            {/* TODO: Fix for undefined book images */}
+            <div>
+                <b>{props.title}</b>
+                <div>{props.author}</div>
+                {/* TODO: Handle multiple authors */}
+                <div>&copy; {props.year} </div>
+                <div>ISBN-13:</div>
+                <div>{props.isbn}</div>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <label>
+                Pick your favorite flavor:
+                {/* <select value={shelf} onChange={e => console.log(e.target.value)}> */}
+                <select value={shelf} onChange={e => setShelf(e.target.value)}>
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
--- src/Components/search.js
+++ src/Components/search.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Booktile from './booktile';
 
 export default class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          keyword: ''
+          keyword: '',
+          searchRes: []
         };
     
         this.handleChange = this.handleChange.bind(this);
@@ -17,21 +19,38 @@ export default class Search extends Component {
       }
     
       handleSubmit(event) {
-        alert(this.state.keyword + 'results logged to console')
+        // alert(this.state.keyword + 'results logged to console')
         event.preventDefault();
-        axios.get("https://www.googleapis.com/books/v1/volumes?q="+this.state.keyword).then(res => console.log(res.data.items))
+        // axios.get("https://www.googleapis.com/books/v1/volumes?q="+this.state.keyword).then(res => console.log(res.data.items[0].volumeInfo.imageLinks.smallThumbnail))
+        axios.get("https://www.googleapis.com/books/v1/volumes?q="+this.state.keyword)
+        .then(res => 
+          this.setState({searchRes: res.data.items})  
+          // console.log(res.data.items)
+          // year: publishedDate YYYY-MM-DD
+          // ISBN: industryIdentifiers[1].identifier ([0] is ISBN 10 not ISBN 13)
+        );
       }
     
       render() {
         return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Search by book title: 
-              <input type="text" name="keyword" value={this.state.keyword} onChange={this.handleChange} />
-            </label>
-            <br />
-            <input type="submit" value="Submit" />
-          </form>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Search by book title: 
+                <input type="text" name="keyword" value={this.state.keyword} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+            <div className="book-search">
+            {this.state.searchRes.map(book => 
+            <Booktile key={book.volumeInfo.industryIdentifiers[1].identifier} 
+              title={book.volumeInfo.title} 
+              author={book.volumeInfo.authors} 
+              year={book.volumeInfo.publishedDate.substring(0,4)} 
+              isbn={book.volumeInfo.industryIdentifiers[1].identifier}
+              imgURL={book.volumeInfo.imageLinks.smallThumbnail}/>)}
+            </div>
+          </div>
         );
       }
     }
