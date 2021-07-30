048fe0dde584d37d2d1b68939a617b38ca86ed6d
Madison Bowden <melybowden@gmail.com>
2021-07-30 12:03:52 -0400

    cleanup library

--- src/Components/library.js
+++ src/Components/library.js
@@ -1,40 +1,10 @@
 import React, { Component } from 'react'
-import Booktile from './booktile'
 import Header from './header'
-// import flickity from 'flickity'
 import Flickity from 'flickity';
 export default class Library extends Component {
     constructor(props){
         super(props)
-        // Do GET
-        this.state={ booklist: [
-            {
-                "title":"Harry Potter",
-                "author":"JK Rowling",
-                "year":1998
-            },
-            {
-                "title":"To Kill a Mockingbird",
-                "author":"Harper Lee",
-                "year":1960
-            },
-            {
-                "title":"Lord of the Rings",
-                "author":"JRR Tolkien",
-                "year":1954
-            }
-        ]}
-        this.makeFlickity = this.makeFlickity.bind(this);
     }
-
-
-    makeFlickity() {
-      var flkty = new Flickity('.carousel');
-      flkty.next();
-      flkty.select( 3 );
-    }
-
-   
 
     render() {
         return (
@@ -43,11 +13,7 @@ export default class Library extends Component {
               <script src="../node_modules/flickity/dist/flickity.pkgd.min.js"></script>
               
               <Header user={this.props.match.params.user}/>
-              {/* {this.state.booklist.map(book=><Booktile title={book.title} author={book.author} year={book.year}/>)} */}
-              {/* {this.makeFlickity} */}
               <h1>Flickity - half-width cells</h1>
-              {/* <div class="carousel js-flickity" 
-              data-flickity='{ "cellAlign": "left", "contain": true }'> */}
               <div class="main-gallery js-flickity" 
               data-flickity-options='{ "cellAlign": "left", "contain": true, "pageDots": false, "prevNextButtons": false }'>
                 <div class="gallery-cell">first thing here</div>
