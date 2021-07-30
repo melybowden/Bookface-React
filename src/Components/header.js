c7d1746af7fdfb8ea5b786efbb124854e352e3e5
Madison Bowden <melybowden@gmail.com>
2021-07-30 15:00:57 -0400

    update header css

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from '../logo_white.svg'
 import text_logo from '../text_logo.svg'
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
     constructor(props) {
@@ -17,16 +18,20 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <div className="flex-container" style={{paddingTop:'2vh'}}>
-                  <img src={logo} alt="logo" height={80} width={80} />
-                  <img src={text_logo} alt="Bookface" height={80}/>
-                  <h3>Hello, {this.props.user}</h3> 
+                <div className="flex-container" style={{padding:'2vh'}}>
+                  <div className="flex-logo">
+                  <img src={logo} alt="logo" height={40} width={40} />
+                  <img src={text_logo} alt="Bookface" height={40}/>
+                  </div>
+                  <div><Link to="/search" className="header-link">Search</Link></div>
+                  <div><Link to="/library" className="header-link">My Library</Link></div>
+                  <h3 style={{color:'white'}}>Hello, {this.props.user}</h3> 
                 </div>
 
-                <div className="flex-container">
+                {/* <div className="flex-container">
                   <button>Search here</button>
                   <button>My Library</button>
-                </div>
+                </div> */}
             </div>
         )
     }
