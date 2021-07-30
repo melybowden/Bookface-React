b3941ec2348b46c2fe462af322f5809c428372a7
Madison Bowden <melybowden@gmail.com>
2021-07-30 16:13:20 -0400

    flexbox scrolling

--- src/Components/shelf.js
+++ src/Components/shelf.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Flickity from 'flickity';
+// import Flickity from 'flickity';
 
 export default function Shelf(props) {
     const Book = (props) => <div className="book"><img src={props.img} alt="book cover" className="book-tmb"></img><b>{props.title}</b></div>
@@ -7,9 +7,9 @@ export default function Shelf(props) {
     return (
       <div>
       <h3>{props.shelfName}</h3>
-        <div className="shelf">
+        {/* <div className="shelf">
           {props.booklist.map(book => <Book key={book.title} img={book.imageURL} title={book.title}/>)}
-        </div>
+        </div> */}
           {/* <link rel="stylesheet" href="../node_modules/flickity/css/flickity.css" media="screen"/>
           <script src="../node_modules/flickity/dist/flickity.pkgd.min.js"></script>
           <div class="main-gallery js-flickity" 
@@ -27,6 +27,9 @@ export default function Shelf(props) {
               <div class="gallery-cell"></div>
               <div class="gallery-cell"></div>
           </div> */}
+        <div className="scrolling-wrapper-flexbox">
+          {props.booklist.map(book => <div className="card"><Book key={book.title} img={book.imageURL} title={book.title}/></div>)}
+        </div>
       </div>
     )
 }
