6d6e2099e84830cd79bba88f3ec335f547b8c6bf
Nate <nathan.smiley4@gmail.com>
2021-08-02 15:45:04 -0400

    Latest

--- src/Components/shelf.js
+++ src/Components/shelf.js
@@ -1,34 +1,33 @@
 import React from 'react'
-// import Flickity from 'flickity';
+import book_not_found from './book_not_found.jpg'
 
 export default function Shelf(props) {
-    const Book = (props) => <div className="book"><img src={props.img} alt="book cover" className="book-tmb"></img><b>{props.title}</b></div>
+  function coverimage(props){
+  return <div className = "book-tmb" style = {{backgroundColor: "red"}}>{ props.title} by {props.author}</div> 
+
+
+
+
+
+  }
+
+
+
+
+
+
+    const Book = (props) =>
+     <div className="book">
+       {props.img != book_not_found ? <img src={props.img} alt="book cover" className="book-tmb"></img>:coverimage(props)}
+       <b>{props.title}</b>
+       </div>
 
     return (
-      <div>
+      <div className= "shelf">
       <h3>{props.shelfName}</h3>
-        {/* <div className="shelf">
-          {props.booklist.map(book => <Book key={book.title} img={book.imageURL} title={book.title}/>)}
-        </div> */}
-          {/* <link rel="stylesheet" href="../node_modules/flickity/css/flickity.css" media="screen"/>
-          <script src="../node_modules/flickity/dist/flickity.pkgd.min.js"></script>
-          <div class="main-gallery js-flickity" 
-            data-flickity-options='{ "cellAlign": "left", "contain": true, "pageDots": false, "prevNextButtons": false }'>
-              {props.booklist.map(book => <div class="gallery-cell"></div>)}
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-              <div class="gallery-cell"></div>
-          </div> */}
+        
         <div className="scrolling-wrapper-flexbox">
-          {props.booklist.map(book => <div className="card"><Book key={book.title} img={book.imageURL} title={book.title}/></div>)}
+          {props.booklist.map(book => <div className="card"><Book key={book.title} img={book.imageURL} title={book.title } author= {book.author}/></div>)}
         </div>
       </div>
     )
