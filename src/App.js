aa52296981312b764f5de55271456d80a681ced6
Madison Bowden <melybowden@gmail.com>
2021-08-03 08:46:02 -0400

    add sessionStorage auth

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ import Search from './Components/search';
 import React, { useState } from 'react';
 import Login from './Components/login';
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken)); 
+  // Expects {token: 'test123'}
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
 
   if(!token) {
     return <Login setToken={setToken} />
