8d1560f9b9960b100bd6705280aeb92723d966bd
Madison Bowden <melybowden@gmail.com>
2021-08-03 09:16:13 -0400

    refactor header to functional

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,31 +3,47 @@ import logo from '../logo_white.svg'
 import text_logo from '../text_logo.svg'
 import { NavLink } from 'react-router-dom'
 
-export default class Header extends Component {
-    constructor(props) {
-      super(props)
-
-      this.state = {
-        username: '',
-        password:''
-      }
-
-    }
-
-
-    render() {
-        return (
-            <div className="header">
-                <div className="flex-container" style={{padding:'2vh'}}>
-                  <div className="flex-logo">
-                  <img src={logo} alt="logo" height={40} width={40} />
-                  <img src={text_logo} alt="Bookface" height={40}/>
-                  </div>
-                  <div activeClassName="active" className="header-link"><NavLink to="/search">Search</NavLink></div>
-                  <div activeClassName="active" className="header-link"><NavLink to="/library">My Library</NavLink></div>
-                  <h3 style={{color:'white'}}>Hello, {this.props.user}</h3> 
-                </div>
-            </div>
-        )
-    }
+export default function Header(props) {
+  return (
+    <div className="header">
+        <div className="flex-container" style={{padding:'2vh'}}>
+          <div className="flex-logo">
+            <img src={logo} alt="logo" height={40} width={40} />
+            <img src={text_logo} alt="Bookface" height={40}/>
+          </div>
+          <div activeClassName="active" className="header-link"><NavLink to="/search">Search</NavLink></div>
+          <div activeClassName="active" className="header-link"><NavLink to="/library">My Library</NavLink></div>
+          <h3 style={{color:'white'}}>Hello, {props.user}</h3> 
+        </div>
+    </div>
+  )
 }
+
+// export default class Header extends Component {
+//     constructor(props) {
+//       super(props)
+
+//       this.state = {
+//         username: '',
+//         password:''
+//       }
+
+//     }
+
+
+//     render() {
+//         return (
+//             <div className="header">
+//                 <div className="flex-container" style={{padding:'2vh'}}>
+//                   <div className="flex-logo">
+//                   <img src={logo} alt="logo" height={40} width={40} />
+//                   <img src={text_logo} alt="Bookface" height={40}/>
+//                   </div>
+//                   <div activeClassName="active" className="header-link"><NavLink to="/search">Search</NavLink></div>
+//                   <div activeClassName="active" className="header-link"><NavLink to="/library">My Library</NavLink></div>
+//                   <h3 style={{color:'white'}}>Hello, {this.props.user}</h3> 
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
