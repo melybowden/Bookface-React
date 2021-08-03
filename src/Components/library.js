e30f5064a48a039db245d43cd3842e7227145483
Madison Bowden <melybowden@gmail.com>
2021-08-03 17:59:06 -0400

    update library and shelfify to add user shelves from api

--- src/Components/library.js
+++ src/Components/library.js
@@ -1,46 +1,33 @@
 import React, { Component } from 'react'
 import Header from './header';
 import axios from 'axios';
-import Shelf from './shelf';
+import Shelfify from './shelfify';
+// import Shelf from './shelf';
 export default class Library extends Component {
     constructor(props){
         super(props)
         this.state = {
-          booklist: [], 
-          shelfview: null
+          booklist: []
         }
-        this.getShelves = this.getShelves.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
 
     componentDidMount() {
+      // console.log("mounted")
+      // console.log("https://cygnus-bookface.herokuapp.com/bookface/user/"+this.props.match.params.user)
       axios.get("https://cygnus-bookface.herokuapp.com/bookface/user/"+this.props.match.params.user)
       .then(res =>
         this.setState({booklist: res.data})
       );
     }
 
-    // getShelves(books) {
-    //   var shelves = {"Other": []}
-    //   for (var b in books) {
-    //     if (books[b].shelf && shelves.includes(books[b].shelf)) {
-    //       shelves[books[b].shelf].push(books[b])
-    //     }
-    //     else if (books[b].shelf) {
-    //       shelves[books[b].shelf] = [books[b]]
-    //     }
-    //     else {
-    //       shelves["Other"].push(books[b])   // books[b] has no shelf name
-    //     }
-    //   }
-    //   console.log(shelves)
-    //   return shelves                        // returns dict of shelf name: book array
-    // }
-
     render() {
         return (
             <div style={{overflow:'hidden'}}>
               <Header user={this.props.match.params.user}/>
-              {this.getShelves(this.state.booklist).map(s => console.log(s))}
+              <Shelfify books={this.state.booklist} />
+              {/* {console.log(this.state.booklist)} */}
+              {/* {this.getShelves(this.state.booklist).map(s => console.log(s))} */}
               {/* <Shelf shelfName="Book Club" booklist={this.state.booklist} />
               <Shelf shelfName="Summer Reading List" booklist={this.state.booklist} /> */}
             </div>
