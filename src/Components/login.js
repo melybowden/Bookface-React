17e54737c896748ef62073c79ca8997158151cba
Madison Bowden <melybowden@gmail.com>
2021-08-04 08:38:00 -0400

    refactor login as rfc and update to use session storage

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import logo from '../login_logo.svg';
 
-export default class login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password:''
-    };
+async function loginUser(credentials) {
+  console.log("login")
+  return axios.post('https://cygnus-bookface.herokuapp.com/users/login', credentials)
+    .then(res => 
+      // console.log(res)
+      res.data
+    ) // {first: "user created?logged in?failed?", second: {username:"",displayname:"",id:#, loggedIn:bool,password:""}}
+ }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+export default function Login({setToken}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    console.log(token)
+    setToken(token);
   }
-
-  handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    axios.post('https://cygnus-bookface.herokuapp.com/users/login',this.state)
-  .then(results => 
-    console.log(results)
-    // this.props.setToken(results)
-    // alert(results.data.first)
-    )
-  .catch(error => console.log("error: " + error))
-  }
-
-  render() {
-    return (
+  
+  return (
       <div className="lockscreen">
         <img style={{height:"100%"}} src={logo} alt="BookFace-Logo" />
         <div className="login-card">
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <h1 style={{color: '#1877F2'}}>Login</h1>
-            <input type="text" name="username"  placeholder="Enter Username" value={this.state.username} onChange={this.handleChange} />
+            <input type="text" name="username"  placeholder="Enter Username" value={username} onChange={e => setUsername(e.target.value)} />
             <br />
-            <input type="password" name="password" placeholder="Enter Password" value={this.state.password} onChange={this.handleChange} />
+            <input type="password" name="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
             <br />
             <input type="submit" value="Submit" />
           </form>
@@ -48,6 +43,55 @@ export default class login extends Component {
           </div>
         </div>
       </div>
-    );
-  }
+  )
 }
+
+// export default class login extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       username: '',
+//       password:''
+//     };
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) {
+//     this.setState({[event.target.name]: event.target.value});
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     axios.post('https://cygnus-bookface.herokuapp.com/users/login',this.state)
+//     .then(results => 
+//       // useToken(results.data.second)
+//       console.log(results)
+//     // this.props.setToken(results)
+//     // alert(results.data.first)
+//     )
+//   .catch(error => console.log("error: " + error))
+//   }
+
+//   render() {
+//     return (
+      // <div className="lockscreen">
+      //   <img style={{height:"100%"}} src={logo} alt="BookFace-Logo" />
+      //   <div className="login-card">
+      //     <form onSubmit={this.handleSubmit}>
+      //       <h1 style={{color: '#1877F2'}}>Login</h1>
+      //       <input type="text" name="username"  placeholder="Enter Username" value={this.state.username} onChange={this.handleChange} />
+      //       <br />
+      //       <input type="password" name="password" placeholder="Enter Password" value={this.state.password} onChange={this.handleChange} />
+      //       <br />
+      //       <input type="submit" value="Submit" />
+      //     </form>
+      //     <div style={{marginTop: "30%"}}>
+      //       <Link to="/createuser" >New User? Create Account Here</Link>
+      //     </div>
+      //   </div>
+      // </div>
+//     );
+//   }
+// }
