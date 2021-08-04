c105167215d1dce8cc40528ece7cf0e80c2502ec
Katelyn Shepard <katelyn.shepard@gmail.com>
2021-08-04 16:17:47 -0400

    Changing styling of header and search page

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,12 +2,15 @@ import React from 'react'
 import logo from '../logo_white.svg'
 import text_logo from '../text_logo.svg'
 import { NavLink, useParams } from 'react-router-dom'
+import { BiSearch } from 'react-icons/bi'
+import { ImBooks } from 'react-icons/im'
+import { BiUserCircle } from 'react-icons/bi'
 
 export default function Header(props) {
   let {user} = useParams();
   return (
     <div className="header">
-        <div className="flex-container" style={{padding:'2vh'}}>
+        <div className="flex-container" justify content="space-between" style={{padding:'3vh'}}>
           <div className="flex-logo">
             <img src={logo} alt="logo" height={40} width={40} />
             <img src={text_logo} alt="Bookface" height={40}/>
@@ -16,9 +19,9 @@ export default function Header(props) {
             <li><NavLink to="/search" className="header-link" activeClassName="active">Search</NavLink></li>
             <li><NavLink to="/library" className="header-link" activeClassName="active">My Library</NavLink></li>
           </ul> */}
-          <h3 className="header-link"><NavLink to={"/search/"+user} activeClassName="active">Search</NavLink></h3>
-          <h3 className="header-link"><NavLink to={"/library/"+user} activeClassName="active">My Library</NavLink></h3>
-          <h3 style={{color:'white'}}>Hello, {props.user}</h3> 
+          <h3 className="header-link"><NavLink to={"/search/"+user} activeClassName="active"><BiSearch />  Search</NavLink></h3>
+          <h3 className="header-link"><NavLink to={"/library/"+user} activeClassName="active"><ImBooks />  My Library</NavLink></h3>
+          <h3 style={{color:'white'}}><BiUserCircle />  Hello, {props.user}</h3> 
         </div>
     </div>
   )
